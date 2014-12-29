e50f38cae9bce97351a0a9efca0aa3279127546d
yzxsunshine <yzxsunshine@gmail.com>
2014-12-29 11:04:23 -0500

    Drag and Drop
    1. Add dragging and dropping event to stars

---
+++ 3DUIContest/Assets/Scripts/SceneControl.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SceneControl : MonoBehaviour {
+	//private SensorFusion senfu = null;
+	private Rect ctrlPanel;
+	private Rect scenePanel;
+	private List<Vector2> starList;
+	// Use this for initialization
+	void Start () {
+		//senfu = new SensorFusion();
+		//senfu.Start();
+		CreateScenePanel();
+		CreateCtrlPanel();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//senfu.Update();
+	}
+
+	void OnGUI ()
+	{
+		//GUI.Label(new Rect(10, 10, 500, 20), senfu.Orientation.ToString());
+		// End the Groups and Area	
+		Event e = Event.current;
+		if (e.isKey)
+			Debug.Log("Detected key code: " + e.keyCode);	
+		DrawCtrlPanel();
+	}
+
+	void CreateCtrlPanel() {
+		float widthUnit = Screen.width * 1.0f / 16;
+		float heightUnit = Screen.height * 1.0f / 9;
+		ctrlPanel = new Rect(widthUnit * 11.5f, heightUnit * 0.5f, widthUnit * 4, heightUnit * 8);
+		Vector3 pos = new Vector3((ctrlPanel.x + 50) / Screen.width, (ctrlPanel.y + ctrlPanel.height - 50) / Screen.height, 0);
+		GameObject star = Instantiate(Resources.Load("Prefab/starGUI", typeof(GameObject)), pos, Quaternion.identity) as GameObject;
+		star.guiTexture.pixelInset.Set(-25, -25, 50, 50);
+	}
+
+	void DrawCtrlPanel() {
+		GUI.Box(ctrlPanel, "haha");
+
+	}
+
+	void CreateScenePanel() {
+		float widthUnit = Screen.width * 1.0f / 16;
+		float heightUnit = Screen.height * 1.0f / 9;
+		scenePanel = new Rect(0, 0, widthUnit * 11.5f, Screen.height);
+		StarCtrl.scenePanel = scenePanel;
+	}
+}
---
+++ 3DUIContest/Assets/Scripts/StarCtrl.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// part of the code is from http://wiki.unity3d.com/index.php?title=DraggableGUIElement
+
+public enum STAR_STATUS {
+	IN_PANEL,
+	FOLLOWING,
+	IN_SCENE
+};
+
+public class StarCtrl : MonoBehaviour {
+	Vector3 lastMousePosition;
+	Rect border;
+	STAR_STATUS status;
+
+	public static Rect scenePanel;
+	// Use this for initialization
+	void Start () {
+		border = new Rect(0, 0, Screen.width, Screen.height);
+		status = STAR_STATUS.IN_PANEL;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnMouseDown() {
+		lastMousePosition = GetClampedMousePosition();
+		status = STAR_STATUS.FOLLOWING;
+		Instantiate(Resources.Load("Prefab/starGUI", typeof(GameObject)), this.transform.position, this.transform.rotation);
+	}
+
+	void OnMouseUp() {
+		if(scenePanel.Contains(Input.mousePosition)) {
+			status = STAR_STATUS.IN_SCENE;
+		}
+		else {
+			Destroy(this.gameObject);
+		}
+	}
+
+	Vector3 GetClampedMousePosition()
+	{
+		Vector3 mousePosition = Input.mousePosition;
+		mousePosition.x = Mathf.Clamp(mousePosition.x, 0f, Screen.width);
+		mousePosition.y = Mathf.Clamp(mousePosition.y, 0f, Screen.height);
+		
+		return mousePosition;
+	}
+
+	void OnMouseDrag()
+	{
+		Vector3 delta = GetClampedMousePosition() - lastMousePosition;
+		
+		delta = Camera.main.ScreenToViewportPoint(delta);
+		
+		transform.position += delta;
+		
+		Vector3 position = transform.position;
+		position.x = Mathf.Clamp(position.x, border.x, border.x + border.width);
+		position.y = Mathf.Clamp(position.y, border.y, border.y + border.height);
+		
+		transform.position = position;
+		
+		lastMousePosition = GetClampedMousePosition();
+	}
+}
